1b74d56ba1637fe921b88ec739d5874f74100e08
Amiel Bornales <amubawrnaw@gmail.com>
2021-02-24 23:45:01 +0800

    fixed (?) installability error

--- sw.js
+++ sw.js
@@ -1,3 +1,8 @@
 self.addEventListener('install', (e) => {
     console.log('Installed');
+    
+});
+this.addEventListener('fetch', function (event) {
+    // it can be empty if you just want to get rid of that error
+    //purely for installability lmfao
 });
